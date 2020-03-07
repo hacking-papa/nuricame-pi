d75766273132c06ffc449c8605413ea13ae65315
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2020-03-08 06:32:22 +0900

    :wrench: Refactor Printer.py

--- Printer.py
+++ Printer.py
@@ -69,203 +69,202 @@ class BtCommandByte(object):
 
 
 class Printer:
-    standardKey = 0x35769521
+    standard_key = 0x35769521
     padding_line = 300
     max_send_msg_length = 2016
     max_recv_msg_length = 1024
     uuid = "00001101-0000-1000-8000-00805F9B34FB"
 
     def __init__(self, address=None):
+        self.sock = None
         self.address = address
+        self.service = None
         self.crc_key_set = False
-        self.connected = True if self.connect() else False
+        self.crc_key = None
+        self.connected = self.connect()
 
     def connect(self):
         if self.address is None and not self.scan_devices():
             return False
         if not self.scan_services():
             return False
-        logging.info("Service found. Connecting to \"%s\" on %s..." % (self.service["name"], self.service["host"]))
+        logging.info(f"Service found. Connecting to {self.service['name']} on {self.service['host']}.")
         self.sock = BluetoothSocket(RFCOMM)
         self.sock.connect((self.service["host"], self.service["port"]))
         self.sock.settimeout(60)
         logging.info("Connected.")
-        self.registerCrcKeyToBt()
+        self.register_crc_key2bluetooth()
         return True
 
     def disconnect(self):
         try:
             self.sock.close()
-        except:
+        except OSError:
             pass
         logging.info("Disconnected.")
 
     def scan_devices(self):
-        logging.warning("Searching for devices...")
-        valid_names = ['MiaoMiaoJi', 'Paperang']
+        logging.warning("Searching for devices ...")
+        valid_names = ["MiaoMiaoJi", "Paperang"]
         nearby_devices = discover_devices(lookup_names=True)
         valid_devices = list(filter(lambda d: len(d) == 2 and d[1] in valid_names, nearby_devices))
-        if len(valid_devices) == 0:
-            logging.error("Cannot find device with name %s." % " or ".join(valid_names))
+        if not len(valid_devices):
+            logging.error(f"Cannot find device with name {' or '.join(valid_names)}.")
             return False
-        elif len(valid_devices) > 1:
-            logging.warning("Found multiple valid machines, the first one will be used.\n")
+        elif 1 < len(valid_devices):
+            logging.warning(f"Found multiple valid machines, the first one will be used.\n")
             logging.warning("\n".join(valid_devices))
         else:
-            logging.warning(
-                "Found a valid machine with MAC %s and name %s" % (valid_devices[0][0], valid_devices[0][1])
-            )
+            logging.warning(f"Found a valid machine with MAC {valid_devices[0][0]} and name {valid_devices[0][1]}.")
         self.address = valid_devices[0][0]
         return True
 
     def scan_services(self):
-        logging.info("Searching for services...")
+        logging.info("Searching for services ...")
         service_matches = find_service(uuid=self.uuid, address=self.address)
-        print(service_matches)
+        logging.debug(service_matches)
         valid_service = list(filter(
-            lambda s: 'protocol' in s and 'name' in s and s['protocol'] == 'RFCOMM' and s['name'] == 'SerialPort',
-            service_matches
-        ))
-        if len(valid_service) == 0:
-            logging.error("Cannot find valid services on device with MAC %s." % self.address)
+            lambda s: "protocol" in s and "name" in s and s["protocol"] == "RFCOMM" and s["name"] == "SerialPort",
+            service_matches))
+        if not len(valid_service):
+            logging.error(f"Cannot find valid services on device with MAC {self.address}.")
             return False
         logging.info("Found a valid service on target device.")
         self.service = valid_service[0]
         return True
 
-    def sendMsgAllPackage(self, msg):
+    def send_message_all_package(self, msg):
         # Write data directly to device
         sent_len = self.sock.send(msg)
-        logging.info("Sending msg with length = %d..." % sent_len)
+        logging.info(f"Sending msg with length = {sent_len}")
 
     def crc32(self, content):
-        return zlib.crc32(content, self.crcKey if self.crc_key_set else self.standardKey)
+        return zlib.crc32(content, self.crc_key if self.crc_key_set else self.standard_key)
 
-    def packPerBytes(self, bytes, control_command, i):
-        result = struct.pack('<BBB', 2, control_command, i)
-        result += struct.pack('<H', len(bytes))
+    def pack_per_bytes(self, bytes, control_command, i):
+        result = struct.pack("<BBB", 2, control_command, i)
+        result += struct.pack("<H", len(bytes))
         result += bytes
-        result += struct.pack('<I', self.crc32(bytes))
-        result += struct.pack('<B', 3)
+        result += struct.pack("<I", self.crc32(bytes))
+        result += struct.pack("<B", 3)
         return result
 
-    def addBytesToList(self, bytes):
+    def add_bytes2list(self, bytes):
         length = self.max_send_msg_length
         result = [bytes[i:i + length] for i in range(0, len(bytes), length)]
         return result
 
-    def sendToBt(self, allbytes, control_command, need_reply=True):
-        bytes_list = self.addBytesToList(allbytes)
+    def send2bluetooth(self, all_bytes, control_command, need_reply=True):
+        bytes_list = self.add_bytes2list(all_bytes)
         for i, bytes in enumerate(bytes_list):
-            tmp = self.packPerBytes(bytes, control_command, i)
-            self.sendMsgAllPackage(tmp)
+            tmp = self.pack_per_bytes(bytes, control_command, i)
+            self.send_message_all_package(tmp)
         if need_reply:
-            return self.receive()
+            return self.recv()
 
-    def receive(self):
+    def recv(self):
         # Here we assume that there is only one received packet.
         raw_msg = self.sock.recv(self.max_recv_msg_length)
-        parsed = self.resultParser(raw_msg)
-        logging.info("Recv: " + raw_msg.hex())
-        logging.info("Received %d packets: " % len(parsed) + "".join([str(p) for p in parsed]))
+        parsed = self.result_parser(raw_msg)
+        logging.info(f"Receive: {raw_msg.hex()}")
+        logging.info(f"Received {len(parsed)} packets: {''.join([str(p) for p in parsed])}")
         return raw_msg, parsed
 
-    def resultParser(self, data):
+    def result_parser(self, data):
         base = 0
-        res = []
-        while base < len(data) and data[base] == '\x02':
+        result = []
+        while base < len(data) and data[base] == "\x02":
             class Info(object):
                 def __str__(self):
-                    return "\nControl command: %s(%s)\nPayload length: %d\nPayload(hex): %s" % (
-                        self.command, BtCommandByte.find_command(self.command)
-                        , self.payload_length, self.payload.hex()
-                    )
+                    return f"""
+                        Control command: {self.command}({BtCommandByte.find_command(self.command)})
+                        Payload length: {self.payload_length}
+                        Payload(hex): {self.payload.hex()}"""
 
             info = Info()
-            _, info.command, _, info.payload_length = struct.unpack('<BBBH', data[base:base + 5])
+            _, info.command, _, info.payload_length = struct.unpack("<BBBH", data[base:base + 5])
             info.payload = data[base + 5: base + 5 + info.payload_length]
             info.crc32 = data[base + 5 + info.payload_length: base + 9 + info.payload_length]
             base += 10 + info.payload_length
-            res.append(info)
-        return res
+            result.append(info)
+        return result
 
-    def registerCrcKeyToBt(self, key=0x6968634 ^ 0x2e696d):
-        logging.info("Setting CRC32 key...")
-        msg = struct.pack('<I', int(key ^ self.standardKey))
-        self.sendToBt(msg, BtCommandByte.PRT_SET_CRC_KEY)
-        self.crcKey = key
+    def register_crc_key2bluetooth(self, key=0x6968634 ^ 0x2e696d):
+        logging.info("Setting CRC32 key ...")
+        msg = struct.pack("<I", int(key ^ self.standard_key))
+        self.send2bluetooth(msg, BtCommandByte.PRT_SET_CRC_KEY)
+        self.crc_key = key
         self.crc_key_set = True
         logging.info("CRC32 key set.")
 
-    def sendPaperTypeToBt(self, paperType=0):
-        # My guess:
+    def send_paper_type2bluetooth(self, paper_type=0):
         # paperType=0: normal paper
         # paperType=1: official paper
-        msg = struct.pack('<B', paperType)
-        self.sendToBt(msg, BtCommandByte.PRT_SET_PAPER_TYPE)
+        msg = struct.pack("<B", paper_type)
+        self.send2bluetooth(msg, BtCommandByte.PRT_SET_PAPER_TYPE)
 
-    def sendPowerOffTimeToBt(self, poweroff_time=0):
-        msg = struct.pack('<H', poweroff_time)
-        self.sendToBt(msg, BtCommandByte.PRT_SET_POWER_DOWN_TIME)
+    def send_power_off_time2bluetooth(self, power_off_time=0):
+        msg = struct.pack("<H", power_off_time)
+        self.send2bluetooth(msg, BtCommandByte.PRT_SET_POWER_DOWN_TIME)
 
-    def sendBinaryToBt(self, binary_img):
-        self.sendPaperTypeToBt()
+    def send_binary2bluetooth(self, binary_img):
+        self.send_paper_type2bluetooth()
         # msg = struct.pack("<%dc" % len(binary_img, *binary_img)
-        msgs = [binary_img[x: x + 192] for x in range(0, len(binary_img), 192)]  # 4*48
+        msgs = [binary_img[x: x + 192] for x in range(0, len(binary_img), 192)]  # 4 * 48
         for msg in msgs:
-            self.sendToBt(msg, BtCommandByte.PRT_PRINT_DATA, need_reply=False)
-        self.sendFeedLineToBt(self.padding_line)
+            self.send2bluetooth(msg, BtCommandByte.PRT_PRINT_DATA, need_reply=False)
+        self.send_feed_line2bluetooth(self.padding_line)
 
-    def sendImageToBt(self, binary_img):
-        self.sendPaperTypeToBt()
-        print(binary_img)
+    def send_image2bluetooth(self, binary_img):
+        self.send_paper_type2bluetooth()
+        logging.debug(binary_img)
         height, width = binary_img.shape[:]
         for line in range(height):
             bits = [0 if x > 0 else 1 for x in binary_img[line]]
             bits = [bits[x:x + 8] for x in range(0, len(bits), 8)]
-            msg = ''
+            msg = ""
             for bit in bits:
-                bin = '0b' + ''.join(str(x) for x in bit)
-                msg += '{:02x}'.format(int(bin, 0))
+                bin = "0b" + "".join(str(x) for x in bit)
+                msg += "{:02x}".format(int(bin, 0))
             msg = bytes.fromhex(msg)
-            self.sendToBt(msg, BtCommandByte.PRT_PRINT_DATA, need_reply=False)
-        self.sendFeedLineToBt(self.padding_line)
+            self.send2bluetooth(msg, BtCommandByte.PRT_PRINT_DATA, need_reply=False)
+        self.send_feed_line2bluetooth(self.padding_line)
 
-    def sendSelfTestToBt(self):
-        msg = struct.pack('<B', 0)
-        self.sendToBt(msg, BtCommandByte.PRT_PRINT_TEST_PAGE)
+    def send_self_test2bluetooth(self):
+        msg = struct.pack("<B", 0)
+        self.send2bluetooth(msg, BtCommandByte.PRT_PRINT_TEST_PAGE)
 
-    def sendDensityToBt(self, density):
-        msg = struct.pack('<B', density)
-        self.sendToBt(msg, BtCommandByte.PRT_SET_HEAT_DENSITY)
+    def send_density2bluetooth(self, density):
+        msg = struct.pack("<B", density)
+        self.send2bluetooth(msg, BtCommandByte.PRT_SET_HEAT_DENSITY)
 
-    def sendFeedLineToBt(self, length):
-        msg = struct.pack('<H', length)
-        self.sendToBt(msg, BtCommandByte.PRT_FEED_LINE)
+    def send_feed_line2bluetooth(self, length):
+        msg = struct.pack("<H", length)
+        self.send2bluetooth(msg, BtCommandByte.PRT_FEED_LINE)
 
-    def queryBatteryStatus(self):
-        msg = struct.pack('<B', 1)
-        self.sendToBt(msg, BtCommandByte.PRT_GET_BAT_STATUS)
+    def query_battery_status(self):
+        msg = struct.pack("<B", 1)
+        self.send2bluetooth(msg, BtCommandByte.PRT_GET_BAT_STATUS)
 
-    def queryDensity(self):
-        msg = struct.pack('<B', 1)
-        self.sendToBt(msg, BtCommandByte.PRT_GET_HEAT_DENSITY)
+    def query_density(self):
+        msg = struct.pack("<B", 1)
+        self.send2bluetooth(msg, BtCommandByte.PRT_GET_HEAT_DENSITY)
 
-    def sendFeedToHeadLineToBt(self, length):
-        msg = struct.pack('<H', length)
-        self.sendToBt(msg, BtCommandByte.PRT_FEED_TO_HEAD_LINE)
+    def send_feed_to_head_line2bluetooth(self, length):
+        msg = struct.pack("<H", length)
+        self.send2bluetooth(msg, BtCommandByte.PRT_FEED_TO_HEAD_LINE)
 
-    def queryPowerOffTime(self):
-        msg = struct.pack('<B', 1)
-        self.sendToBt(msg, BtCommandByte.PRT_GET_POWER_DOWN_TIME)
+    def query_power_off_time(self):
+        msg = struct.pack("<B", 1)
+        self.send2bluetooth(msg, BtCommandByte.PRT_GET_POWER_DOWN_TIME)
 
-    def querySNFromBt(self):
-        msg = struct.pack('<B', 1)
-        self.sendToBt(msg, BtCommandByte.PRT_GET_SN)
+    def query_sn_from_bluetooth(self):
+        msg = struct.pack("<B", 1)
+        self.send2bluetooth(msg, BtCommandByte.PRT_GET_SN)
 
-    def queryHardwareInfo(self):
-        msg = struct.pack('<B', 1)
-        self.sendToBt(msg, BtCommandByte.PRT_GET_HW_INFO)
+    def query_hardware_info(self):
+        msg = struct.pack("<B", 1)
+        self.send2bluetooth(msg, BtCommandByte.PRT_GET_HW_INFO)
 
 
 if __name__ == "__main__":
@@ -273,18 +272,19 @@ if __name__ == "__main__":
     printer = Printer()
 
     if printer.connected:
-        printer.sendDensityToBt(95)
+        printer.send_density2bluetooth(95)
 
         # Print an existing image(need opencv):
-        img = cv2.imread('sample_images/sample_1920x1920.jpg', 0)
-        ret, binary_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+        img = cv2.imread("sample_images/sample_1920x1920.jpg", 0)
+        # ret, binary_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+        binary_img, _ = cv2.decolor(img)
         height, width = binary_img.shape[:]
         binary_img = cv2.resize(binary_img, (384, int(height * 384.0 / width)), cv2.INTER_AREA)
-        printer.sendImageToBt(binary_img)
+        printer.send_image2bluetooth(binary_img)
 
         # Print a pure black image with 300 lines
-        img = b'\xff' * 48 * 300
-        printer.sendBinaryToBt(img)
+        img = b"\xff" * 48 * 300
+        printer.send_binary2bluetooth(img)
 
         printer.disconnect()
     else:
