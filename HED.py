34d604a0f2d57d1b86dc855c8e944b6159c91a4e
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2020-03-16 18:07:48 +0900

    :+1: Add HED.py

---
+++ HED.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8-*-
+
+import argparse
+
+import cv2 as cv
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", help="Path to input image.", required=True)
+parser.add_argument("--width", help="Resize input image to a specific width.", default=384, type=int)
+parser.add_argument("--height", help="Resize input image to a specific height.", default=384, type=int)
+parser.add_argument("--output", help="Path to output image.", default="output.jpg")
+args = parser.parse_args()
+
+
+class CropLayer(object):
+    def __init__(self, params, blobs):
+        self.x_start = 0
+        self.x_end = 0
+        self.y_start = 0
+        self.y_end = 0
+
+    def getMemoryShapes(self, inputs):
+        input_shape, target_shape = inputs[0], inputs[1]
+        batch_size, num_channels = input_shape[0], input_shape[1]
+        height, width = target_shape[2], target_shape[3]
+
+        self.y_start = int((input_shape[2] - target_shape[2]) / 2)
+        self.x_start = int((input_shape[3] - target_shape[3]) / 2)
+        self.y_end = self.y_start + height
+        self.x_end = self.x_start + width
+
+        return [[batch_size, num_channels, height, width]]
+
+    def forward(self, inputs):
+        return [inputs[0][:, :, self.y_start:self.y_end, self.x_start:self.x_end]]
+
+
+net = cv.dnn.readNetFromCaffe("deploy.prototxt", "hed_pretrained_bsds.caffemodel")
+cv.dnn_registerLayer("Crop", CropLayer)
+
+image = cv.imread(args.input)
+image = cv.resize(image, (args.width, args.height))
+input_blob = cv.dnn.blobFromImage(image, scalefactor=1.0, size=(args.width, args.height),
+                                  mean=(104.00698793, 116.66876762, 122.67891434),
+                                  swapRB=False, crop=False)
+net.setInput(input_blob)
+output = net.forward()
+
+output = output[0, 0]
+output = cv.resize(output, (image.shape[1], image.shape[0]))
+
+output = cv.cvtColor(output, cv.COLOR_GRAY2BGR)
+output = 255 * output
+output = output.astype(np.uint8)
+
+print(f"np.max(output): {np.max(output)}")
+print(f"np.min(output): {np.min(output)}")
+print(f"output.shape: {output.shape}")
+
+cv.imwrite(args.output, output)
